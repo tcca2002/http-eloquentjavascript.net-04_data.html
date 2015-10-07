9432ea1000823fbf857d7ddc86dbb6db795c6713
tcca2002 <tcca2002@yahoo.com>
2015-10-07 13:22:02 +1100

    Create Excercise1.js

---
+++ Chapter4/Excercise1.js
@@ -0,0 +1,19 @@
+// Your code here.
+var range = function(start,end)
+{
+
+myarray=[];
+for (var i=start; i<=end; i++)
+myarray.push("i");
+};
+
+var sum = function(range(start,end))
+{
+var mysum=0;
+for (var i=1; i<=(end-start+1); i++)
+mysum += myaray[i];
+}
+console.log(sum(range(1, 10)));
+// → 55
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
